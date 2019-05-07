cf5a7069107849a003e171ced4b9f7f8fe8c9a6c
alumno <alumno@utnfra.com.ar>
2019-05-07 20:34:24 -0300

    parcial

---
+++ 20190507-Silva.Martin/20190507-Silva.Martin/Maquina.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20190507_Silva.Martin
+{
+    class Maquina
+    {
+        int CantidadMaxPerifericos;
+
+        string nombre;
+
+        List<Periferico> perifericos;
+
+        int CantidadMaximaPerifericos
+        {
+            get { return CantidadMaxPerifericos; }
+            set { CantidadMaxPerifericos = value; }
+        }
+
+        string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        string systemInfo
+        {
+            get { return systemInfo; }
+        }
+
+
+        public enum Edistribucion
+        {
+            Dvorak,
+            QWERTY,
+            QWERTZ,
+            AZERTY
+        }
+    }
+}
---
+++ 20190507-Silva.Martin/GUI/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace GUI
+{
+    partial class FrmPC
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.lblModelo = new System.Windows.Forms.Label();
+            this.lblMemoria = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnSacar = new System.Windows.Forms.Button();
+            this.btnSystemInfo = new System.Windows.Forms.Button();
+            this.txtMarca = new System.Windows.Forms.TextBox();
+            this.txtModelo = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rtxtSystemInfo = new System.Windows.Forms.RichTextBox();
+            this.NudMemoria = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NudMemoria)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Location = new System.Drawing.Point(6, 46);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(37, 13);
+            this.lblMarca.TabIndex = 2;
+            this.lblMarca.Text = "Marca";
+            this.lblMarca.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblModelo
+            // 
+            this.lblModelo.AutoSize = true;
+            this.lblModelo.Location = new System.Drawing.Point(6, 91);
+            this.lblModelo.Name = "lblModelo";
+            this.lblModelo.Size = new System.Drawing.Size(42, 13);
+            this.lblModelo.TabIndex = 3;
+            this.lblModelo.Text = "Modelo";
+            // 
+            // lblMemoria
+            // 
+            this.lblMemoria.AutoSize = true;
+            this.lblMemoria.Location = new System.Drawing.Point(6, 136);
+            this.lblMemoria.Name = "lblMemoria";
+            this.lblMemoria.Size = new System.Drawing.Size(72, 13);
+            this.lblMemoria.TabIndex = 4;
+            this.lblMemoria.Text = "Memoria (MB)";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(157, 180);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(92, 42);
+            this.btnAgregar.TabIndex = 5;
+            this.btnAgregar.Text = "&Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnSacar
+            // 
+            this.btnSacar.Location = new System.Drawing.Point(303, 180);
+            this.btnSacar.Name = "btnSacar";
+            this.btnSacar.Size = new System.Drawing.Size(92, 42);
+            this.btnSacar.TabIndex = 6;
+            this.btnSacar.Text = "&Sacar";
+            this.btnSacar.UseVisualStyleBackColor = true;
+            // 
+            // btnSystemInfo
+            // 
+            this.btnSystemInfo.Location = new System.Drawing.Point(199, 228);
+            this.btnSystemInfo.Name = "btnSystemInfo";
+            this.btnSystemInfo.Size = new System.Drawing.Size(156, 42);
+            this.btnSystemInfo.TabIndex = 7;
+            this.btnSystemInfo.Text = "&System Info";
+            this.btnSystemInfo.UseVisualStyleBackColor = true;
+            // 
+            // txtMarca
+            // 
+            this.txtMarca.Location = new System.Drawing.Point(100, 43);
+            this.txtMarca.Name = "txtMarca";
+            this.txtMarca.Size = new System.Drawing.Size(295, 20);
+            this.txtMarca.TabIndex = 8;
+            // 
+            // txtModelo
+            // 
+            this.txtModelo.Location = new System.Drawing.Point(100, 88);
+            this.txtModelo.Name = "txtModelo";
+            this.txtModelo.Size = new System.Drawing.Size(295, 20);
+            this.txtModelo.TabIndex = 9;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.NudMemoria);
+            this.groupBox1.Controls.Add(this.lblMarca);
+            this.groupBox1.Controls.Add(this.btnSystemInfo);
+            this.groupBox1.Controls.Add(this.btnSacar);
+            this.groupBox1.Controls.Add(this.txtMarca);
+            this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.txtModelo);
+            this.groupBox1.Controls.Add(this.lblMemoria);
+            this.groupBox1.Controls.Add(this.lblModelo);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(422, 313);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Placa de Video";
+            // 
+            // rtxtSystemInfo
+            // 
+            this.rtxtSystemInfo.Location = new System.Drawing.Point(1, 322);
+            this.rtxtSystemInfo.Name = "rtxtSystemInfo";
+            this.rtxtSystemInfo.Size = new System.Drawing.Size(449, 249);
+            this.rtxtSystemInfo.TabIndex = 13;
+            this.rtxtSystemInfo.Text = "";
+            // 
+            // NudMemoria
+            // 
+            this.NudMemoria.Location = new System.Drawing.Point(100, 136);
+            this.NudMemoria.Name = "NudMemoria";
+            this.NudMemoria.Size = new System.Drawing.Size(295, 20);
+            this.NudMemoria.TabIndex = 10;
+            // 
+            // FrmPC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 571);
+            this.Controls.Add(this.rtxtSystemInfo);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FrmPC";
+            this.Text = "Silva.Martin.2D";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.NudMemoria)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.Label lblModelo;
+        private System.Windows.Forms.Label lblMemoria;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnSacar;
+        private System.Windows.Forms.Button btnSystemInfo;
+        private System.Windows.Forms.TextBox txtMarca;
+        private System.Windows.Forms.TextBox txtModelo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox rtxtSystemInfo;
+        private System.Windows.Forms.NumericUpDown NudMemoria;
+    }
+}
+
